7eca9a76d216d17ed570cd43be288c07baca68ca
jalowe13 <jalowe13@gmail.com>
2025-01-04 16:21:00 -0600

    0.3 - Random Shuffled Card

--- Wargame.py
+++ Wargame.py
@@ -1,18 +1,20 @@
 import pygame
 import typing
+import random
 
-SCREEN_HEIGHT: int = 1280
-SCREEN_WIDTH: int = 720
+SCREEN_HEIGHT: int = 1920 
+SCREEN_WIDTH: int = 1080
 SCREEN_BACKGROUND_COLOR: tuple[int,int,int] = (53,101,77)
-TITLE_NAME: str = "WarGame"
+TITLE_NAME: str = "WarBoard"
 MAJOR: str = str(0)
-MINOR: str = str(2)
+MINOR: str = str(3)
 PATCH: str = str(0)
 TITLE: str = TITLE_NAME + " v." + MAJOR + "." + MINOR + "." + PATCH
 
 # Global Game State Object
 class Game:
     def __init__(self):
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((SCREEN_HEIGHT ,SCREEN_WIDTH))
         self.running: bool = False 
         self.mouseHeld: bool 
@@ -22,6 +24,11 @@ class Game:
         self.running = running
     def get_running(self) -> bool:
         return self.running
+
+    def get_clock(self):
+        return self.clock
+    def get_fps(self): # Get the total time of the running game
+        return self.clock.get_fps()
     def get_screen(self): # Returns Pygame Screen
         return self.screen
     def get_pressed(self) -> bool:
@@ -33,23 +40,63 @@ class Game:
         self.mouseHeld = mouseState.get_pressed()[0]
         self.mousePos = mouseState.get_pos()
 
+
+class Deck:
+    def __init__(self):
+        self.suits: array[str] = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        self.ranks: array[str] = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
+        self.cards: array[Card] = []
+        # Generate pairs
+        for s in self.suits:
+            for r in self.ranks:
+                card = Card(r,s,0,0)
+                self.cards.append(card)
+
+        self.shuffle()
+    def shuffle(self):
+        print("Shuffling...")
+        random.shuffle(self.cards)
+        self.show_deck()
+
+    def show_deck(self):
+        for c in self.cards:
+            print(c.get_info())
+
+    def draw_card(self):
+        card = self.cards.pop()
+        card.update_cords(SCREEN_HEIGHT/2,SCREEN_WIDTH/2)
+        print("The card drawn was ", card.get_info())
+        print("There are now ", len(self.cards), " left")
+        return card
+
 # Card that can be played
-# TODO: Typing on Card Class
 class Card:
-    def __init__(self, x:float, y:float):
-        self.x = x
-        self.y = y
-        self.color = (255,0,0)
-        self.size_x = 64 
-        self.size_y = 89 
-        self.x_diff = self.size_x/2
-        self.y_diff = self.size_y/2
-        self.x_min = self.x - self.x_diff
-        self.x_max = self.x + self.x_diff
-        self.y_min = self.y - self.y_diff
-        self.y_max = self.y + self.y_diff
+    def __init__(self, rank:str, suit:str, x:float, y:float):
+        self.rank: str = rank
+        self.suit: str = suit
+        self.x: float = x
+        self.y: float = y
+        self.color: tuple[int,int,int] = (255,0,0)
+        self.size_x: int = 64 
+        self.size_y: int = 89 
+        self.x_diff: float = self.size_x/2
+        self.y_diff: float = self.size_y/2
+        self.x_min: float = self.x - self.x_diff
+        self.x_max: float = self.x + self.x_diff
+        self.y_min: float = self.y - self.y_diff
+        self.y_max: float = self.y + self.y_diff
+    def get_info(self):
+        return f"{self.rank} of {self.suit}"
     def draw(self,screen): # Draw card on the screen
+        # Card Background
         pygame.draw.rect(screen, self.color, (self.x,self.y,self.size_x,self.size_y))
+        font = pygame.font.SysFont("Arial", 30)  # Use Arial font at size 30
+        suit_text = font.render(self.suit, True, (255, 255, 255))  # Render text in white
+        rank_text = font.render(self.rank, True, (255, 255, 255))  # Render text in white
+             
+        screen.blit(suit_text, (self.x, self.y))  # Draw text at position (10, 10)
+        screen.blit(rank_text, (self.x, self.y + 50))  # Draw text at position (10, 10)
+        # Card Face
     def update(self): # Update card bounding box position 
         self.x_min = self.x 
         self.x_max = self.x + (2*self.x_diff)
@@ -78,12 +125,19 @@ def setup():
     game.set_running(True)
     return game 
 
+
+def display_fps(game,screen,font):
+    fps = str(int(game.get_fps()))
+    fps_text = font.render(fps, True, (255, 255, 255))  # Render text in white
+    screen.blit(fps_text, (10, 10))  # Draw text at position (10, 10)
+
 def main():
     game = setup()
     screen = game.get_screen()    
+    font = pygame.font.SysFont("Arial", 30)  # Use Arial font at size 30
     running = game.get_running() 
-    c: Card = Card(500,500)
-    c1: Card = Card(200,200)
+    deck = Deck()
+    c: Card = deck.draw_card()
 
     # Game Loop
     while running:
@@ -96,9 +150,6 @@ def main():
             # Card press detection needs to be generalized
             if pressed and c.in_range(mouseX,mouseY):
                 c.update_cords(mouseX, mouseY)
-            if pressed and c1.in_range(mouseX,mouseY):
-                c1.update_cords(mouseX, mouseY)
-            
             # Global and Menu Detection
             if event.type == pygame.QUIT:
                 running = False
@@ -106,13 +157,13 @@ def main():
 
             # NOTE: Game Objects Update Function
             c.update()
-            c1.update()
 
         # NOTE: Game Objects Draw Function 
         
         # Update all Cards
         c.draw(screen)
-        c1.draw(screen)
+        display_fps(game,screen,font)
         pygame.display.flip()
+        game.get_clock().tick(144)
 
 main()
