a233fb9fdf25dd8a845ecee3bb4ecd4ba0af00e3
jalowe13 <jalowe13@gmail.com>
2025-01-05 17:16:16 -0600

    0.4 - Player card set

--- Wargame.py
+++ Wargame.py
@@ -7,7 +7,7 @@ SCREEN_WIDTH: int = 1080
 SCREEN_BACKGROUND_COLOR: tuple[int,int,int] = (53,101,77)
 TITLE_NAME: str = "WarBoard"
 MAJOR: str = str(0)
-MINOR: str = str(3)
+MINOR: str = str(4)
 PATCH: str = str(0)
 TITLE: str = TITLE_NAME + " v." + MAJOR + "." + MINOR + "." + PATCH
 
@@ -42,10 +42,12 @@ class Game:
 
 
 class Deck:
-    def __init__(self):
-        self.suits: array[str] = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    def __init__(self, suits: [str]):
+        self.suits: array[str] = suits 
+        # self.suits: array[str] = ["Life", "Currency", "Attack"]
         self.ranks: array[str] = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
         self.cards: array[Card] = []
+        self.draw_primary = True # Determines positioning of next draw of card, toggles each draw
         # Generate pairs
         for s in self.suits:
             for r in self.ranks:
@@ -63,17 +65,35 @@ class Deck:
             print(c.get_info())
 
     def draw_card(self):
+        # Could draw based on card type
         card = self.cards.pop()
-        card.update_cords(SCREEN_HEIGHT/2,SCREEN_WIDTH/2)
+        if self.draw_primary:  
+            #self.draw_primary = False
+            if card.get_suit() == "Attack":
+                card.update_cords(SCREEN_WIDTH/2 + 2*card.x_max,SCREEN_HEIGHT/4)
+            elif card.get_suit() == "Life":
+                card.update_cords(SCREEN_WIDTH/2,SCREEN_HEIGHT/4)
+            elif card.get_suit() == "Currency":
+                card.update_cords(SCREEN_WIDTH/2 + card.x_max,SCREEN_HEIGHT/3)
+        print(card.get_cords())
+        """"
+        else:
+            self.draw_primary = True
+            if card.get_suit() == "Attack":
+                card.update_cords(SCREEN_WIDTH + 2*card.x_max,SCREEN_HEIGHT/4)
+            if card.get_suit() == "Life":
+                card.update_cords(SCREEN_WIDTH,SCREEN_HEIGHT/4)
+            elif card.get_suit() == "Currency":
+                card.update_cords(SCREEN_WIDTH + card.x_max,SCREEN_HEIGHT/3)
+        """
         print("The card drawn was ", card.get_info())
         print("There are now ", len(self.cards), " left")
         return card
-
 # Card that can be played
 class Card:
     def __init__(self, rank:str, suit:str, x:float, y:float):
         self.rank: str = rank
-        self.suit: str = suit
+        self.suit: str = suit  
         self.x: float = x
         self.y: float = y
         self.color: tuple[int,int,int] = (255,0,0)
@@ -85,8 +105,12 @@ class Card:
         self.x_max: float = self.x + self.x_diff
         self.y_min: float = self.y - self.y_diff
         self.y_max: float = self.y + self.y_diff
+    def get_suit(self):
+        return self.suit
     def get_info(self):
         return f"{self.rank} of {self.suit}"
+    def get_cords(self):
+        return f"{self.x},{self.y}"
     def draw(self,screen): # Draw card on the screen
         # Card Background
         pygame.draw.rect(screen, self.color, (self.x,self.y,self.size_x,self.size_y))
@@ -110,8 +134,6 @@ class Card:
             if self.y_min <= y <= self.y_max:
                 return True
         return False 
-
-
 def setup():
     print("Start setup")
     try:
@@ -124,20 +146,30 @@ def setup():
     print("Setup complete")
     game.set_running(True)
     return game 
-
-
+# Display the fps 
 def display_fps(game,screen,font):
     fps = str(int(game.get_fps()))
     fps_text = font.render(fps, True, (255, 255, 255))  # Render text in white
     screen.blit(fps_text, (10, 10))  # Draw text at position (10, 10)
-
 def main():
     game = setup()
     screen = game.get_screen()    
     font = pygame.font.SysFont("Arial", 30)  # Use Arial font at size 30
     running = game.get_running() 
-    deck = Deck()
-    c: Card = deck.draw_card()
+    attack_deck = Deck(["Attack"]) 
+    life_deck = Deck(["Life"])
+    currency_deck = Deck(["Currency"])
+    
+
+    player_cards: array[Card] = [] 
+    # Generate set of attack, life, and currency cards
+    c: Card = attack_deck.draw_card()
+    c1: Card = life_deck.draw_card()
+    c2: Card = currency_deck.draw_card()
+    player_cards.append(c)
+    player_cards.append(c1)
+    player_cards.append(c2)
+
 
     # Game Loop
     while running:
@@ -148,20 +180,24 @@ def main():
             mouseX, mouseY = game.get_mouse()
 
             # Card press detection needs to be generalized
-            if pressed and c.in_range(mouseX,mouseY):
-                c.update_cords(mouseX, mouseY)
+            print(len(player_cards))
+            for c in player_cards:
+                if pressed and c.in_range(mouseX,mouseY):
+                    c.update_cords(mouseX, mouseY)
+                c.update()
+                print("Card updated", c.get_cords(), c.get_info())
             # Global and Menu Detection
             if event.type == pygame.QUIT:
                 running = False
             screen.fill(SCREEN_BACKGROUND_COLOR)
 
             # NOTE: Game Objects Update Function
-            c.update()
 
         # NOTE: Game Objects Draw Function 
         
         # Update all Cards
-        c.draw(screen)
+        for c in player_cards:
+            c.draw(screen)
         display_fps(game,screen,font)
         pygame.display.flip()
         game.get_clock().tick(144)
